b44d7e283611e3e704aae84b08fb4cccf0f90055
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-12-11 14:58:07 +0300

    Learning routes

--- app.py
+++ app.py
@@ -3,5 +3,34 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
+    return 'Index Page'
+
+@app.route('/hi')
+def hello():
     return 'Hello, World!'
+
+#Variable Rules
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #Show the user profile for that user
+    return 'Welcome %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post #%d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
