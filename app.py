64d9cd320b35da24dbe6d05840a21bcd25555573
ckchivatsi <kelvinchivatsi@hotmail.com>
2019-03-19 19:37:50 +0300

    user login and file upload

--- app.py
+++ app.py
@@ -38,11 +38,40 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    #error = None
     if request.method == 'POST':
-        return 'LoggedIn'
-    else:
-        return 'Login Page'
+        #when request method is POST
+        # if valid_login(request.form['username'],
+        #                 request.form['password']):
+        #     return log_the_user_in(request.form['username'])
+        # else:
+        #     error = 'Invalid username/password'
+        return 'LoggedIn as %s' % request.form['username']
+    #when the request method is GET or invalid logins provided
+    #return render_template('login.html', error=error)
+    #return 'Login Page displayed at this point'
+    return '''
+            <form method="post">
+                <p><input type=text name=username>
+                <p><input type=submit value=Login>
+            </form>
+        '''
 
+@app.route('/upload', methods=['GET', 'POST'])
+#Just make sure not to forget to set the enctype="multipart/form-data" 
+#attribute on your HTML form, else no file will be uploaded
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/uploads/uploadedFile.txt')
+        return 'file saved'
+        #If you want to use the filename of the client to store the file on the server,
+        #pass it through the secure_filename() function that Werkzeug provides
+        #import it ==>  from werkzeug.utils import secure_filename
+        #
+        #f.save('/uploads/' + secure_filename(f.filename))
+    return 'no download'
+    
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('show_user_profile', username='ck chai'))
